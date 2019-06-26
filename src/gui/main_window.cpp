8d638e4a24800399c6bb603c130c33f3921f0e4f
nagaina <36309533+nagaina@users.noreply.github.com>
2019-06-26 18:08:33 +0400

    API for statusBar

--- src/gui/main_window.cpp
+++ src/gui/main_window.cpp
@@ -4,6 +4,7 @@
 #include "create_shape_gui.hpp"
 #include "pen_brush_gui.hpp"
 #include "icons.hpp"
+#include "statusbar_manager.hpp"
 
 #include "../commands/command_manager.hpp"
 #include "../commands/gui_commands.hpp"
@@ -66,6 +67,9 @@ main_window::main_window(QWidget* p)
 	setWindowIcon(main_window_icon);
 	QCoreApplication::instance()->installEventFilter(this);
 	command_manager::get_instance()->set_main_widget(this);
+
+	StatusBarManager& sBar = StatusBarManager::getInstance();
+	sBar.setStatusBar(statusBar());
 }
 
 void main_window::make_connections()
@@ -78,4 +82,10 @@ void main_window::make_connections()
 	connect(m_shapes, SIGNAL(reset()), m_canvas, SLOT(reset()));
 	connect(m_shapes, SIGNAL(close()), m_canvas, SLOT(close()));
 	connect(m_shapes, SIGNAL(selectByRegion()), m_canvas, SLOT(invoke_select_by_region()));
+}
+
+main_window::~main_window()
+{
+	StatusBarManager& sBar = StatusBarManager::getInstance();
+	sBar.clear();
 }
---
+++ src/gui/statusbar_manager.hpp
@@ -0,0 +1,45 @@
+#ifndef STATUS_BAR_MANAGER_HPP
+#define STATUS_BAR_MANAGER_HPP
+
+#include <memory>
+
+#include <QStatusBar>
+#include <QMovie>
+#include <QLabel>
+
+//
+//	class StatusBarManager : Singtone
+//	Manages StatusBar in MainWindow, allows to change status everywhere
+//
+class StatusBarManager
+{
+public:
+	//
+	//	Methods
+	//
+	static StatusBarManager& getInstance();
+	//	Initialize with StatusBar
+	void setStatusBar(QStatusBar*);
+	//	Clear the content, call from closing application
+	void clear();
+
+	void updateStatusBar(const QString&, bool error, bool processing);
+
+	~StatusBarManager();
+private:
+	//
+	//	Private helpers
+	// 
+	StatusBarManager();
+
+private:
+	//
+	//	Contents
+	//
+	static std::unique_ptr<StatusBarManager>	m_pInstance;
+	QStatusBar*									m_pStatusBar = nullptr;
+	QMovie*										m_pGif = nullptr;
+	QLabel*										m_pStatusBarLabel = nullptr;
+	QLabel*										m_pGifLabel = nullptr;
+};
+#endif
