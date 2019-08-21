598a4441a95eb1088ff703b988d1b926a3e5bc56
levibyte <kartol@gmx.com>
2019-08-21 11:08:29 +0200

    fixes interative load/save

--- src/commands/interactive_load_save.hpp
+++ src/commands/interactive_load_save.hpp
@@ -26,15 +26,17 @@ class InteractiveDesAction: public InteractiveCommandBase
     
     IObjectPoolPtr m_ws;
     std::string m_fn;
+    std::string m_helpstr;
     bool m_is_saved = false;
     
     public:
 	
         InteractiveDesAction<T>(IObjectPoolPtr s ):m_ws(s) { 
+                m_helpstr = sl_action2string(T);
         }
 
         virtual std::string get_name() {
-                return "incmdDesign"+sl_action2string(T);
+                return "incmdDesign"+m_helpstr;
         }
         
         virtual void execute() {
@@ -49,17 +51,16 @@ class InteractiveDesAction: public InteractiveCommandBase
 		
     private:
         void on_commit(const EvType& ev) {
-            m_fn = "test.txt"; 
             if ( T == LOAD  ) {
                 if ( is_agreed_with_user() )  {
+                    m_fn = QFileDialog::getOpenFileName(0,"Load Design","","*.des",0,QFileDialog::DontUseNativeDialog).toStdString();
                     dicmdDesignLoad(m_ws,m_fn).silent_execute();
-                    //m_fn = QFileDialog::getOpenFileName(command_manager::get_instance()->get_main_widget(),"aaa","aaa","*.*").toStdString();
                 }
             } else {
                  if ( is_agreed_with_user() ) {  
-                    //m_fn = QFileDialog::getSaveFileName(command_manager::get_instance()->get_main_widget(),"aaa","aaa","*.*").toStdString();
-                     dicmdDesignSave(m_ws,m_fn).silent_execute();
-                     m_is_saved = true;
+                    m_fn = QFileDialog::getSaveFileName(0,"Save Design","","*.des",0,QFileDialog::DontUseNativeDialog).toStdString();
+                    dicmdDesignSave(m_ws,m_fn).silent_execute();
+                    m_is_saved = true;
                  }
             }
             abort();
@@ -71,7 +72,7 @@ class InteractiveDesAction: public InteractiveCommandBase
         
         bool is_agreed_with_user() {
             //if ( ! is_saved() )    
-                //return mmModalDialog::ask_yn_question("Design"+sl_action2string(T),"Your design has not been saved yet. Do you want to continue with "+sl_action2string(T)+"?");
+                //return mmModalDialog::ask_yn_question("Design"+m_helpstr,"Your design has not been saved yet. Do you want to continue with "+m_helpstr+"?");
             return true;
         }
 
