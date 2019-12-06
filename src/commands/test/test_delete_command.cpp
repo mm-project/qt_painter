3cbbc50448a1d2c5cb4981bceab54c274cb760b1
nagaina <36309533+nagaina@users.noreply.github.com>
2019-12-06 16:28:04 +0400

    compile

--- src/commands/test/test_delete_command.cpp
+++ src/commands/test/test_delete_command.cpp
@@ -33,7 +33,7 @@ IShape* RegionQuery::getShapeUnderPos(QPoint const&) const { return nullptr; }
 
 void ObjectPoolBase::clear() noexcept {}
 std::vector<IShapePtr> ObjectPoolBase::getObjects() const noexcept { std::vector<IShapePtr> res; return res; }
-IShapePtr Design::addObject(IShapePtr s) { return s;}
+IShapePtr ObjectPoolBase::addObject(IShapePtr s) { return s;}
 std::string Design::getName() const noexcept {}
 void Design::dumpToFile(const std::string&) const {}
 void ObjectPoolBase::removeObject(IShapePtr) noexcept {}
