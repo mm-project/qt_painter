7a9d240c2700677ffa77a0b2be479df1be7da2b7
levibyte <kartol@gmx.com>
2019-12-11 22:34:26 +0100

    x3

--- qa/scripts/circle_helper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export PAINTER_SQA_ROOT=~/project/sqa
-git remote rm origin
-git remote add origin http://github.com/mm-project/qt_painter
-git config --global user.name "levibyte"
-git config --global user.email "kartol@gmx.com"
-git fetch
--- qa/scripts/make_diff_html.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-f=$1
-g=$2
-
-cat html_diff.template | sed "s/%fname1%/$g/" | sed "s/%fname2%/$f/" > $f.html
-sed -i "s/%f1%/cat $g/e" $f.html
-sed -i "s/%f2%/cat $f/e" $f.html
