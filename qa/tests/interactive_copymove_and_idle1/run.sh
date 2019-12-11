69819795ede695847fdb6b5b66fcd23741ebb811
levibyte <kartol@gmx.com>
2019-12-11 17:56:49 +0100

    x

---
+++ qa/tests/interactive_copymove_and_idle1/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ -z "$PAINTER_QA_DIR" ]; then
+    echo "please set \$PAINTER_QA_DIR first"
+    exit -1
+fi
+
+source $PAINTER_QA_DIR/scripts/test_setup.sh
+process_options "$@"
+run_test "-replay ../input/replay.log"
+
---
+++ qa/tests/object_properties_generic/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ -z "$PAINTER_QA_DIR" ]; then
+    echo "please set \$PAINTER_QA_DIR first"
+    exit -1
+fi
+
+source $PAINTER_QA_DIR/scripts/test_setup.sh
+extra_comparision "des.lvi"
+process_options "$@"
+
+run_test "-replay ../input/replay.log"
+
+
+
