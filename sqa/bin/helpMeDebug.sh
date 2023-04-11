baa94c04c1361564bbdd17b7bd862ddf6cd88228
SunnyBerlin <kaiserina.agusta.strasse@gmail.com>
2023-04-11 03:06:58 +0200

    improved helper script

--- sqa/bin/helpMeDebug.sh
+++ sqa/bin/helpMeDebug.sh
@@ -15,23 +15,23 @@ export PAINTER_QA_XVFB=1
 echo "Building and running on >$golden_branch<"
 if [ "$need_rebuild" != "" ]; then
 	git checkout $golden_branch
-	cmake .
-	make -j8
-	mv bin/linux/painter bin/linux/painter.golden
+	cmake . &> /dev/null
+	make -j8 &> /dev/null
+	mv bin/linux/painter bin/linux/painter.golden &> /dev/null
 fi
 cd $testname
 export PAINTER_EXE_NAME="painter.golden"
-./run
+./run.sh
 cd -
 
 echo "Building and running on >$debug_branch<"
 if [ "$need_rebuild" != "" ]; then
-	git checkout $debug_branch
-	cmake .
-	make -j8
+	git checkout $debug_branch &> /dev/null
+	cmake . &> /dev/null
+	make -j8 &> /dev/null
 fi
 export PAINTER_EXE_NAME="painter"
 export PAINTER_TEST_OUT_DIRNAME="output_debug"
 cd $testname
-./run
+./run.sh
 cd - 
