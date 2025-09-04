caf498deff0aafafee888881b3fe093918615008
SunnyAlcudia <levsarg88@gmail.com>
2025-09-04 20:36:07 +0200

    wip 1

--- sqa/bin/runUnitTests.sh
+++ sqa/bin/runUnitTests.sh
@@ -44,7 +44,10 @@ fi
 
 for i in `ls $PAINTER_QA_DIR/../unit_test_bin`; do
     total=`expr $total + 1`
-    #cd $PAINTER_QA_DIR/../unit_test_bin
+    if [ -d $PAINTER_QA_DIR/../unit_test_bin/$i ]; then
+        continue
+    fi
+        cd $PAINTER_QA_DIR/../unit_test_bin
         echo -ne  "Running $PAINTER_QA_DIR/../unit_test_bin/$i --- "
         a=`$PAINTER_QA_DIR/../unit_test_bin/$i &> uttest.info `
         r=$?
