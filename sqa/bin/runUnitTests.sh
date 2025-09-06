a9412083e41fc091011b8398383985ecc3e45d5e
SunnyAlcudia <levsarg88@gmail.com>
2025-09-06 07:32:16 +0200

    undo extra changes

--- sqa/bin/runUnitTests.sh
+++ sqa/bin/runUnitTests.sh
@@ -47,9 +47,9 @@ for i in `ls $PAINTER_QA_DIR/../unit_test_bin`; do
     if [ -d $PAINTER_QA_DIR/../unit_test_bin/$i ]; then
         continue
     fi
-        rm -rf $PAINTER_QA_DIR/../unit_test_out
-        mkdir -p $PAINTER_QA_DIR/../unit_test_out
-        cd $PAINTER_QA_DIR/../unit_test_out
+        #rm -rf $PAINTER_QA_DIR/../unit_test_out
+        #mkdir -p $PAINTER_QA_DIR/../unit_test_out
+        #cd $PAINTER_QA_DIR/../unit_test_out
         echo -ne  "Running $PAINTER_QA_DIR/../unit_test_bin/$i --- "
         a=`$PAINTER_QA_DIR/../unit_test_bin/$i &> uttest.info `
         r=$?
