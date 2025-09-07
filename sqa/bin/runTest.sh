f3359680a99bf3fd0e9f1b8153c81263976ee853
SunnyAlcudia <levsarg88@gmail.com>
2025-09-07 16:03:05 +0200

    wip2

--- sqa/bin/runTest.sh
+++ sqa/bin/runTest.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
-
 test=$1
+rest="${@:2}"
+
 if [ "$test" == "" ]; then
     echo "error: please provide path to test"
     exit 1
 fi
 
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+export PAINTER_QA_DIR=$DIR/..
 cd $test
-./run.sh 
+./run.sh "$rest"
