b29615c5dae9e77e520cbea8ca3a9a06796736c2
SunnyAlcudia <levsarg88@gmail.com>
2025-09-23 01:27:03 +0200

    task-282 wip5

--- sqa/bin/runTestsParallel.sh
+++ sqa/bin/runTestsParallel.sh
@@ -121,7 +121,7 @@ function printFailedTests
     failed_tests_files=$(find $wdir -name .failed_tsts* )
     rm -f FAILURES.html
     fail_id=1
-    url_prefix="sqa/tests"
+    url_prefix="sqa/tests/"
     if [ "$CI_CHECK" == "1" ]; then
         url_prefix=""
     fi
@@ -131,7 +131,7 @@ function printFailedTests
         cat $f | sed 's/^/\t/'
         for failed_test in $failures; do
             failure=$(echo $failed_test | cut -d/ -f2-)
-            echo "$fail_id: <a href=\"$url_prefix/$failure/output/DIFF.html\"> $failure </a><br><br>" >> FAILURES.html
+            echo "$fail_id: <a href=\"${url_prefix}${failure}/output/DIFF.html\"> $failure </a><br><br>" >> FAILURES.html
             fail_id=$(expr $fail_id + 1)
         done
     done
