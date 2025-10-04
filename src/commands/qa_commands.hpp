e47f0de83eb5ff1ae2bb63facd1bdb97e3cf196e
SunnyAlcudia <levsarg88@gmail.com>
2025-10-04 22:49:59 +0200

    wip14

--- src/commands/qa_commands.hpp
+++ src/commands/qa_commands.hpp
@@ -219,15 +219,15 @@ bool check_pics(const QString &file1, const QString &file2)
 bool are_imagefiles_different(const QString &file1, const QString &file2)
 {
     //first check per pixel
-    //if (are_imagefiles_different_per_pixel(file1,file2))
+    if (are_imagefiles_different_per_pixel(file1,file2))
         //if different per pixel do more sophisiticated comparisions
-        //return are_imagefiles_different_python_magic(file1,file2);
+        return are_imagefiles_different_python_magic(file1,file2);
 
-    check_pics(file1, file2);
-    are_imagefiles_different_per_pixel(file1,file2);
-    are_imagefiles_different_python_magic(file1,file2);
+    //check_pics(file1, file2);
+    //are_imagefiles_different_per_pixel(file1,file2);
+    //are_imagefiles_different_python_magic(file1,file2);
 
-    return true;
+    return false;
 }
 
 bool are_two_files_different(qaCompType type, const QString &file1, const QString &file2)
